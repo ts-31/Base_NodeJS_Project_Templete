25ae72ab615f85fbcc02dddc8f8ce35d8c4cb053
ts-31 <104221666+ts-31@users.noreply.github.com>
2025-03-04 15:30:42 +0530

    Added basic express functionalities

---
+++ src/config/logger-config.js
@@ -0,0 +1,20 @@
+const { cretateLogger, format, transports, createLogger } = require('winston');
+const { combine, timestamp, label, printf } = format;
+
+const customFormat = printf(({ level, message, timestamp }) => {
+  return `${timestamp} ${level}: ${message}`;
+});
+
+const logger = createLogger({
+  format: combine(
+    timestamp({format: 'YYYY-MM-DD:mm:ss'}),
+    customFormat
+  ),
+  transports: [
+    new transports.Console(),
+    new transports.File({ filename: 'combined.log' })
+  ]
+});
+
+module.exports = logger;
+  
